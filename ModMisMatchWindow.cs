77f30453132f2a83250e76fa6e6937fd52a3b16f
zzzz465 <zzzz465@naver.com>
2019-09-23 18:40:29 +0900

    c

--- ModMisMatchWindow.cs
+++ ModMisMatchWindow.cs
@@ -10,32 +10,59 @@ namespace ModComparerPatch
     public class ModMisMatchWindow : Window
     {
         static ElementContainer ModElementContainer = new ElementContainer(ContainerType.Active);
+		Vector2 SingleItemSize = new Vector2(40, 20);
+
+		public override Vector2 InitialSize
+		{
+			get
+			{
+				return new Vector2(700, 800);
+			}
+		}
+
+		
         public ModMisMatchWindow()
         {
 
         }
-        public override void DoWindowContents(Rect inRect)
+        public override void DoWindowContents(Rect canvas)
         {
-            //inRect는 기본 사각형 크기
-            string name = null; // 할당해주자
-            Rect Title = new Rect(inRect.x + 30, inRect.y - 30, Text.CalcSize(name).x, Text.CalcSize(name).y);
-            Widgets.Label(Title, name);
+			/*
+			//inRect는 기본 사각형 크기
+			Rect outRect = new Rect(canvas.xMin, canvas.yMin, canvas.width, canvas.height);
+			Texture2D texture = new Texture2D((int)outRect.width, (int)outRect.height);
+			texture.SetPixel(0, 0, Color.white);
+			texture.Apply();
+			GUI.skin.box.normal.background = texture;
+			GUI.Box(outRect, GUIContent.none);
+			int itemCount = 50;
+			Rect inRect = new Rect(canvas.xMin, canvas.yMin, 80, itemCount * 50f);
 
-            Rect SaveOutRect = new Rect(inRect.x + 10, inRect.x - inRect.height + 20, 40f, 60f); // 겉 테두리
-            Rect viewRect = new Rect(0f, 0f, SaveOutRect.width - 16f, 300);
-            Widgets.BeginScrollView(SaveOutRect, ref this.ScrollPosition, viewRect, true);
-            float yPos = 6f;
-            float num2 = this.ScrollPosition.y - 30f; // ?
-            float num3 = this.ScrollPosition.y + SaveOutRect.height;
-            foreach (ModElement modElement in ModElementContainer.elementList)
-            {
-                Rect singleModRect = new Rect(0f, yPos, viewRect.width, 30f);
-                Widgets.Label(singleModRect, "Test");
-                yPos += 30f;
-            }
-            Widgets.EndScrollView();
-            
-        }
+			Rect button = new Rect(outRect.xMin, outRect.yMin, outRect.width, 50f);
+			Widgets.BeginScrollView(outRect, ref ScrollPosition, inRect, true);
+			for(int i = 0; i < itemCount; i++)
+			{
+				Widgets.Label(button, "Test");
+				button.y += 50f;
+			}
+			Widgets.EndScrollView();*/
+			Rect TextBox = new Rect(canvas.xMin, canvas.yMin, canvas.width, 30f);
+			Rect OutActiveRect = new Rect(canvas.xMin, TextBox.yMin, canvas.width / 2, canvas.height - TextBox.height);
+			int ItemCount = 100; // 나중에 계산해서 둠
+			Rect SingleModItemBox = new Rect(Vector2.zero, SingleItemSize);
+			Rect inActiveRect = new Rect(OutActiveRect.xMin, OutActiveRect.yMin, OutActiveRect.width, SingleModItemBox.height * ItemCount);
+
+			List<ModElement> ActiveMods = new List<ModElement>(); // 다른걸로 초기화해주자.
+			SingleModItemBox.position = new Vector2(inActiveRect.x, )
+			for(int i = 0; i < ItemCount; i++)
+			{
+				//ModElement element = ActiveMods.ElementAt(i); // 이걸로 수정
+				ModElement element = new ModElement("test", 0, true, true);
+				//Placeholder 처리 해주자
+				
+			}
+
+		}
 
         public Vector2 ScrollPosition = Vector2.zero;
     }
