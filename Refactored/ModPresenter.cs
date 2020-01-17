10abc8adcae84089df7a21c318b97c43d0d5610f
Madeline <zzzz465@naver.com>
2020-01-17 12:09:54 +0900

    algorithm fix - 마지막 요소가 빠지던 에러 수정

--- Refactored/ModPresenter.cs
+++ Refactored/ModPresenter.cs
@@ -4,9 +4,9 @@ using System.Linq;
 
 namespace Madeline.ModMismatchFormatter
 {
-    public class ModPresenter : iOrderFormatter
+    public class OrderFormatterImpl : iOrderFormatter
     {
-        public ModPresenter()
+        public OrderFormatterImpl()
         {
 
         }
@@ -26,14 +26,14 @@ namespace Madeline.ModMismatchFormatter
             Mod SaveCurrent = ModsFromSave.Pop();
             Mod ActiveCurrent = ModsFromActive.Pop();
 
-            while(ModsFromSave.Count > 0 && ModsFromActive.Count > 0) // 여기서 요소가 있다는 것을 보장함, 대신 다 끝나고 1개 또는 0개가 남을 수 있음
+            while(SaveCurrent != null && ActiveCurrent != null) // 여기서 요소가 있다는 것을 보장함, 대신 다 끝나고 1개 또는 0개가 남을 수 있음
             {
                 if(SaveCurrent.ModName == ActiveCurrent.ModName) // 동일할경우(그냥 추가하면 됨)
                 {
                     FormattedActiveMods.Add(ActiveCurrent);
                     FormattedSaveMods.Add(SaveCurrent);
-                    SaveCurrent = ModsFromSave.Pop();
-                    ActiveCurrent = ModsFromActive.Pop();
+                    SaveCurrent = ModsFromSave.PopOrNull();
+                    ActiveCurrent = ModsFromActive.PopOrNull();
                     continue;
                 }
 
@@ -51,13 +51,13 @@ namespace Madeline.ModMismatchFormatter
                     {
                         FormattedActiveMods.Add(ActiveCurrent);
                         FormattedSaveMods.Add(new Mod());
-                        ActiveCurrent = ModsFromActive.Pop();
+                        ActiveCurrent = ModsFromActive.PopOrNull();
                     }
                     else // Active의 Gap이 더 클경우 Save를 먼저 더해주자.
                     {
                         FormattedSaveMods.Add(SaveCurrent);
                         FormattedActiveMods.Add(new Mod());
-                        SaveCurrent = ModsFromSave.Pop();
+                        SaveCurrent = ModsFromSave.PopOrNull();
                     }
                 }
                 //먼저 Active를 띄운다음 그다음 Save를 
@@ -66,14 +66,14 @@ namespace Madeline.ModMismatchFormatter
                 {
                     FormattedSaveMods.Add(SaveCurrent);
                     FormattedActiveMods.Add(new Mod());
-                    SaveCurrent = ModsFromSave.Pop();
+                    SaveCurrent = ModsFromSave.PopOrNull();
                 }
                 //만약 Active에만 존재할경우
                 else if (ActiveModInSaveModList == null)
                 {
                     FormattedActiveMods.Add(ActiveCurrent);
                     FormattedSaveMods.Add(new Mod());
-                    ActiveCurrent = ModsFromActive.Pop();
+                    ActiveCurrent = ModsFromActive.PopOrNull();
                 }
                 else
                 {
@@ -81,16 +81,18 @@ namespace Madeline.ModMismatchFormatter
                 }
             }
             
-            while(ModsFromSave.Count > 0)
+            while(SaveCurrent != null)
             {
-                FormattedSaveMods.Add(ModsFromSave.Pop());
+                FormattedSaveMods.Add(SaveCurrent);
                 FormattedActiveMods.Add(new Mod());
+                SaveCurrent = ModsFromSave.PopOrNull();
             }
 
-            while(ModsFromActive.Count > 0)
+            while(ActiveCurrent != null)
             {
-                FormattedActiveMods.Add(ModsFromActive.Pop());
+                FormattedActiveMods.Add(ActiveCurrent);
                 FormattedSaveMods.Add(new Mod());
+                ActiveCurrent = ModsFromActive.PopOrNull();
             }
 
             if(FormattedSaveMods.Count != FormattedActiveMods.Count)
