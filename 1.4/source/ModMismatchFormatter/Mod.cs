76886267d91aea35ed7eb0e5b5d05889ee7522e8
madeline <zzzz465@naver.com>
2022-10-22 14:49:07 +0900

    make assembly loadable

---
+++ 1.4/source/ModMismatchFormatter/Mod.cs
@@ -0,0 +1,12 @@
+﻿using Verse;
+
+namespace ModMismatchFormatter
+{
+    public class Mod : Verse.Mod
+    {
+        public Mod(ModContentPack content) : base(content)
+        {
+            Log.Message("Hello World!");
+        }
+    }
+}
