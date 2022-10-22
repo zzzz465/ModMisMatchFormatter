045639e7f3294ef49d0f83f3cf36c80266172b4f
madeline <zzzz465@naver.com>
2022-10-22 15:54:01 +0900

    hook harmony function on ModList drawer function

--- 1.4/source/ModMismatchFormatter/HarmonyPatch.cs
+++ 1.4/source/ModMismatchFormatter/HarmonyPatch.cs
@@ -1,10 +1,11 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 using Verse;
 
 namespace ModMismatchFormatter
@@ -22,16 +23,26 @@ namespace ModMismatchFormatter
 
             Log.Message("Patching Madeline.ModMismatchWindow");
 
-            Harmony HMInstance = new Harmony("Madeline.ModMismatchWindowPatch");
+            Harmony harmony = new Harmony("Madeline.ModMismatchWindowPatch");
             MethodInfo original = AccessTools.Method(typeof(ScribeMetaHeaderUtility), "TryCreateDialogsForVersionMismatchWarnings");
             MethodInfo prefix = AccessTools.Method(typeof(HarmonyPatch), "Prefix_TryCreateDialogForVersionMismatchWarnings");
-            HMInstance.Patch(original, new HarmonyMethod(prefix));
+            harmony.Patch(original, new HarmonyMethod(prefix));
+
+            MethodInfo DoModList_original = AccessTools.Method(typeof(RimWorld.Dialog_ModMismatch), "DoModList");
+            MethodInfo DoModList_prefix = AccessTools.Method(typeof(HarmonyPatch), "Prefix_DoModList");
+            harmony.Patch(DoModList_original, new HarmonyMethod(DoModList_prefix));
         }
 
         static bool Prefix_TryCreateDialogForVersionMismatchWarnings(ref bool __result, Action confirmedAction)
         {
             Verse.Log.Message("TryCreateDIalogForVersionMismatchWarnings called");
             return true;
+        }
+
+        static bool Prefix_DoModList(ref Rect r, ref List<string> modList, ref Vector2 scrollPos, ref Color? rowColor)
+        {
+            Log.Message("prefix_domodlist called");
+            return false;
         }
     }
 }
